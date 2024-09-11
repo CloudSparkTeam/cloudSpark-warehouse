498338cca4456818a6e987631652f92c7e1bad48
Vitaog <vitaoog@gmail.com>
2024-09-11 20:47:09 -0300

    feat: Adição do prisma e estrutura inicial do BD

---
+++ prisma/migrations/20240911234259_init/migration.sql
@@ -0,0 +1,32 @@
+-- CreateTable
+CREATE TABLE "Usuario" (
+    "id" SERIAL NOT NULL,
+    "nome" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "senha" TEXT NOT NULL,
+    "data_criacao" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Usuario_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ImagemSatelite" (
+    "id" SERIAL NOT NULL,
+    "usuario_id" INTEGER NOT NULL,
+    "longitude" DOUBLE PRECISION NOT NULL,
+    "latitude" DOUBLE PRECISION NOT NULL,
+    "data_imagem" TIMESTAMP(3) NOT NULL,
+    "data_download" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "status" TEXT NOT NULL,
+
+    CONSTRAINT "ImagemSatelite_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Usuario_email_key" ON "Usuario"("email");
+
+-- CreateIndex
+CREATE INDEX "ImagemSatelite_usuario_id_idx" ON "ImagemSatelite"("usuario_id");
+
+-- AddForeignKey
+ALTER TABLE "ImagemSatelite" ADD CONSTRAINT "ImagemSatelite_usuario_id_fkey" FOREIGN KEY ("usuario_id") REFERENCES "Usuario"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,25 @@
 import express from 'express';
 import cors from 'cors';
+import { PrismaClient } from '@prisma/client';
 
 const app = express();
 const port = 3002;
 
+// Inicializando o Prisma Client
+const prisma = new PrismaClient();
+
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Backend API está sendo executado!');
+app.get('/', async (req, res) => {
+  try {
+    await prisma.$connect();
+    res.send('Conexão com o banco de dados bem-sucedida! Backend API está sendo executado.');
+  } catch (error) {
+    console.error('Erro ao conectar ao banco de dados:', error);
+    res.status(500).send('Erro ao conectar ao banco de dados.');
+  } finally {
+    await prisma.$disconnect();
+  }
 });
 
 app.listen(port, () => {
