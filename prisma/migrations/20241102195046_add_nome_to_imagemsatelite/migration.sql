fbc2af3d54a2b9d3a9e5842a069d9428d85fcc6b
Igor Pereira <igorpereir28@hotmail.com>
2024-11-02 17:04:51 -0300

    feat: Campo de nome acrescentado na tabela ImagemSatelite

---
+++ prisma/migrations/20241102195046_add_nome_to_imagemsatelite/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - Added the required column `nome` to the `ImagemSatelite` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "ImagemSatelite" ADD COLUMN     "nome" TEXT NOT NULL;
